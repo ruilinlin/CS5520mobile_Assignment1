ec50bc138d9c8ba848d75d43b90dde4b44d93af9
ruilinsun <garrolinlin@gmail.com>
2024-01-28 12:20:08 -0800

    add basic frame of finalscreens

---
+++ GuessGame/screens/FinalScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import Card from './components/Card';
+
+export default function FinalScreen({ userWon, onRestartGame, goalNumber }) {
+  const imageUrl = userWon ? `https://picsum.photos/id/${goalNumber}/100/100` : require('./img/Sad-Face-Emoji.png');
+
+  return (
+    <View style={styles.screen}>
+      <Card style={styles.card}>
+        {userWon ? (
+          <Text>Here is your picture</Text>
+        ) : (
+          <Text>Sorry, better luck next time!</Text>
+        )}
+        <Image source={imageUrl} style={styles.image} />
+        <Button title="Start Again" onPress={onRestartGame} />
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: { 
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  card: { 
+    padding: 20, 
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  }
+});
