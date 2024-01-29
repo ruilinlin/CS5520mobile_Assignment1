bbf7fcee88c3cad647d1afe0df94fbf0e9f476ac
ruilinsun <garrolinlin@gmail.com>
2024-01-29 11:55:13 -0800

    change card back to correct code

--- components/Card.js
+++ components/Card.js
@@ -1,57 +1,21 @@
-import React, { useState ,useEffect} from 'react';
-import { View, Text, StyleSheet, Button, Modal } from 'react-native';
-import Card from '../components/Card';
-import CustomButton from '../components/Button';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
-export default function GameScreen({ name, guess,attemptsLeft,onEndGame,onNewGuess, modalVisible ,goalNumber}) {
-  const [isCorrectGuess, setIsCorrectGuess] = useState(false);
+const CustomCard = ({ children, style }) => {
+  return <View style={{ ...styles.card, ...style }}>{children}</View>;
+};
 
-  useEffect(() => {
-    if (parseInt(guess) === goalNumber) {
-      setIsCorrectGuess(true);
-    }
-  }, [guess, goalNumber]);
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 5,
+    backgroundColor: 'gray',
+    padding: 20,
+    borderRadius: 10,
+  },
+});
 
-  let hint = guess < goalNumber ? 'higher' : 'lower';
-
-
-  return (
-    <Modal visible={modalVisible} animationType="slide">
-      <View style={styles.screen}>
-        <Card style={styles.card}>
-          {isCorrectGuess ? (
-            <>
-              <Text>Congrats {name}! You won!</Text>
-              <CustomButton title="Thank you!" onPress={() => onEndGame(true)} />
-            </>
-          ) : (
-            <>
-              // Inside GameScreen component
-              <Text>Hello {name}, You have chosen {guess}. That's not my number! Guess {hint}! You have {attemptsLeft} attempts left!</Text>
-              <CustomButton title='I am done' onPress={() => onEndGame(true)} />
-              <CustomButton 
-              title="Let me Guess Again" 
-              onPress={() => {onNewGuess}} 
-              disabled={attemptsLeft <= 1}
-              />
-            </>
-          )}
-        </Card>
-      </View>
-    </Modal>
-  );
-}
-
-
-  const styles = StyleSheet.create({
-    screen: { 
-      flex: 1, 
-      alignItems: 'center', 
-      justifyContent: 'center'
-    },
-    card: { 
-      padding: 20, 
-      borderRadius: 10,
-      alignItems: 'center'
-    }
-  });
+export default CustomCard;
