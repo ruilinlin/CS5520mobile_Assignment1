b056d26bd3ec24dc130ea5e1d33e76b5210fa5c9
ruilinlin <106147018+ruilinlin@users.noreply.github.com>
2024-01-29 19:35:45 -0800

    remove pass previous value to there

--- GuessGame/screens/GameScreen.js
+++ GuessGame/screens/GameScreen.js
@@ -2,8 +2,9 @@ import React, { useState ,useEffect} from 'react';
 import { View, Text, StyleSheet, Button, Modal } from 'react-native';
 import Card from '../components/Card';
 import CustomButton from '../components/Button';
+import GradientBackground from '../components/Background';
 
-export default function GameScreen({ userName, userGuess,attemptsLeft,onEndGame,onNewGuess, modalVisible ,goalNumber,previousInput}) {
+export default function GameScreen({ userName, userGuess,attemptsLeft,onEndGame,onNewGuess, modalVisible ,goalNumber}) {
   const [isCorrectGuess, setIsCorrectGuess] = useState(false);
 
   useEffect(() => {
@@ -13,30 +14,34 @@ export default function GameScreen({ userName, userGuess,attemptsLeft,onEndGame,
   }, [userGuess, goalNumber]);
 
   let hint = userGuess < goalNumber ? 'higher' : 'lower';
-
+  console.log(goalNumber);
 
   return (
     <Modal visible={modalVisible} animationType="slide">
-      <View style={styles.screen}>
-        <Card style={styles.card}>
-          {isCorrectGuess ? (
-            <>
-              <Text>Congrats {userName}! You won!</Text>
-              <CustomButton title="Thank you!" onPress={() => onEndGame(true)} />
-            </>
-          ) : (
-            <>
-              <Text>Hello {userName}, You have chosen {userGuess}. That's not my number! Guess {hint}! You have {attemptsLeft - 1} attempts left!</Text>
-              <CustomButton title='I am done' onPress={() => onEndGame(true)} />
-              <CustomButton 
-                title="Let me Guess Again" 
-                onPress={() => onNewGuess(previousInput.name, previousInput.guess)} 
-                disabled={attemptsLeft <= 1}
-              />
-            </>
-          )}
-        </Card>
-      </View>
+      <GradientBackground>
+        <View style={styles.screen}>
+          <Card style={styles.card}>
+            {isCorrectGuess ? (
+              <>
+                <Text>Congrats {userName}! You won!</Text>
+                <CustomButton title="Thank you!" onPress={() => onEndGame(userGuess)} />
+              </>
+            ) : (
+              <>
+                <Text>
+                  Hello {userName}, You have chosen {userGuess}. That's not my number! Guess {hint}! You have {attemptsLeft - 1} attempts left!
+                </Text>
+                <CustomButton title="I am done" onPress={() => onEndGame(userGuess)} />
+                <CustomButton
+                  title="Let me Guess Again"
+                  onPress={() => onNewGuess(userName, userGuess)}
+                  disabled={attemptsLeft <= 1}
+                />
+              </>
+            )}
+          </Card>
+        </View>
+      </GradientBackground>
     </Modal>
   );
 }
