ab9538fea3796edb12a4c0a6bdb824b1e3cdaeb9
ruilinlin <106147018+ruilinlin@users.noreply.github.com>
2024-01-29 19:34:56 -0800

    change theimg selection way

--- GuessGame/screens/FinalScreen.js
+++ GuessGame/screens/FinalScreen.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import Card from '../components/Card';
 import CustomButton from '../components/Button';
+import Header from "../components/Header";
+import GradientBackground from '../components/Background';
 
 export default function FinalScreen({ userWon, onRestart, goalNumber }) {
 
-  console.log(userWon)
   return (
+    <GradientBackground>
     <View style={styles.screen}>
+    <Header title="Game is Over"/>
       <Card style={styles.card}>
+        
       <Text>Here is your picture</Text>
 
         <Image
@@ -18,6 +22,7 @@ export default function FinalScreen({ userWon, onRestart, goalNumber }) {
         <CustomButton title="Start Again" onPress={onRestart} />
       </Card>
     </View>
+    </GradientBackground>
   );
 }
 
