6b335e044d9fe7e4de78a063cb7bbb8ff37c3b98
ruilinlin <106147018+ruilinlin@users.noreply.github.com>
2024-01-29 16:51:38 -0800

    Update Header.js

--- GuessGame/components/Header.js
+++ GuessGame/components/Header.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const header = ({title}) =>(
+const Header = ({title}) =>(
   <View style={styles.headerContainer}>
     <Text style={styles.headerTitle}>{title}</Text>
   </View>
